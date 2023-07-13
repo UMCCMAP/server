548e3535798f4ce499d3efd59829cd4889b45cf1
seongddiyong <kodd1102@naver.com>
2023-07-13 21:20:58 +0900

    [#1] Board: add Comment Entity - drafting

---
+++ cmap/src/main/java/com/umc/cmap/domain/board/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.umc.cmap.domain.board.entity;
+
+import com.umc.cmap.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_idx")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_idx")
+    private User user;
+
+    private String comment_content;
+
+    @Builder
+    public Comment(User user, Board board, String comment_content) {
+        this.board = board;
+        this.user = user;
+        this.comment_content = comment_content;
+    }
+}
