a911f33f1c2fd8429e835c074cb51c73b80f9b68
yeonjy <yeonjypaper@gmail.com>
2023-07-15 01:08:14 +0900

    feat: cafe entity

---
+++ cmap/src/main/java/com/umc/cmap/cafe/entity/Cafe.java
@@ -0,0 +1,29 @@
+package com.umc.cmap.cafe.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cafe {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cafe_idx")
+    private Long idx;
+
+    private String name;
+
+    @Embedded
+    private Coordinate coordinate;
+
+
+    @Builder
+    public Cafe(String name, Coordinate coordinate, String information) {
+        this.name = name;
+        this.coordinate = coordinate;
+    }
+
+}
