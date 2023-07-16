ee7c12004f9415030ec48cef9c02c7060c009235
threeony <threeknot1250@cau.ac.kr>
2023-07-16 23:17:04 +0900

    feat: profile entity

---
+++ cmap/src/main/java/com/umc/cmap/domain/board/users/Profile.java
@@ -0,0 +1,40 @@
+package com.umc.cmap.domain.board.users;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="profile_idx")
+    private Long idx;
+
+    private String user_img;
+
+    private String user_info;
+
+    private String cafe_img;
+
+    private String cafe_info;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_idx")
+    private Users user;
+
+
+    @Builder
+    public Profile(String user_img, String user_info, String cafe_img, String cafe_info, Users user){
+        this.user = user;
+        this.user_img = user_img;
+        this.user_info = user_info;
+        this.cafe_img = cafe_img;
+        this.cafe_info = cafe_info;
+    }
+
+}
