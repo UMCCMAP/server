dab332080f8d3d71955740f05a064969c0758a14
wonseokyoon <01088115030a@gmail.com>
2023-07-18 01:39:12 +0900

    feat: cafe 수정사항

--- cmap/src/main/java/com/umc/cmap/domain/cafe/controller/CafeController.java
+++ cmap/src/main/java/com/umc/cmap/domain/cafe/controller/CafeController.java
@@ -5,7 +5,6 @@ import com.umc.cmap.config.BaseResponse;
 import com.umc.cmap.domain.cafe.entity.Cafe;
 import com.umc.cmap.domain.cafe.service.CafeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
