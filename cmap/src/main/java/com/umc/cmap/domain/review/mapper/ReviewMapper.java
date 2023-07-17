97deefaf490e9d8d0c3d564d6b0c3a6ead145e24
yeonjy <yeonjypaper@gmail.com>
2023-07-18 01:51:27 +0900

    refactor: add images parameter to toResponse

--- cmap/src/main/java/com/umc/cmap/domain/review/mapper/ReviewMapper.java
+++ cmap/src/main/java/com/umc/cmap/domain/review/mapper/ReviewMapper.java
@@ -2,12 +2,14 @@ package com.umc.cmap.domain.review.mapper;
 
 import com.umc.cmap.domain.review.dto.ReviewResponse;
 import com.umc.cmap.domain.review.entity.Review;
+import com.umc.cmap.domain.review.entity.ReviewImage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ReviewMapper {
-    ReviewResponse toResponse(Review review);
+    ReviewResponse toResponse(Review review, List<ReviewImage> images);
 }
