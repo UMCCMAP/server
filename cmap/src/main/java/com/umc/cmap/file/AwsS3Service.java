67f142072be076839ad346defe1d42abe78ec37c
yeonjy <yeonjypaper@gmail.com>
2023-07-20 02:02:35 +0900

    feat: add return image url

--- cmap/src/main/java/com/umc/cmap/file/AwsS3Service.java
+++ cmap/src/main/java/com/umc/cmap/file/AwsS3Service.java
@@ -1,7 +1,6 @@
 package com.umc.cmap.file;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -10,12 +9,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -30,8 +29,8 @@ public class AwsS3Service {
 
     private final AmazonS3 amazonS3;
 
-    public List<String> uploadFile(List<MultipartFile> multipartFile) {
-        List<String> fileNameList = new ArrayList<>();
+    public List<URL> uploadFile(List<MultipartFile> multipartFile) {
+        List<URL> imageUrls = new ArrayList<>();
 
         multipartFile.forEach(file -> {
             String fileName = createFileName(file.getOriginalFilename());
@@ -41,10 +40,10 @@ public class AwsS3Service {
             } catch(IOException e) {
                 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
             }
-            fileNameList.add(fileName);
+            imageUrls.add(getUrl(fileName));
         });
 
-        return fileNameList;
+        return imageUrls;
     }
 
     public void deleteFile(String fileName) {
@@ -64,9 +63,14 @@ public class AwsS3Service {
     }
 
     private ObjectMetadata createMetadata(MultipartFile file) {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(file.getSize());
-        objectMetadata.setContentType(file.getContentType());
-        return objectMetadata;
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+        return metadata;
     }
+
+    private URL getUrl(String fileName) {
+        return amazonS3.getUrl(bucket, fileName);
+    }
+
 }
