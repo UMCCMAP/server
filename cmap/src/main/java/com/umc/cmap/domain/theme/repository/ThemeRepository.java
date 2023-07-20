4dd438f98c2b9ae02ef1d62a07be885a2f34a735
wonseokyoon <01088115030a@gmail.com>
2023-07-20 20:23:40 +0900

    feat:add [cafe]theme.repository

---
+++ cmap/src/main/java/com/umc/cmap/domain/theme/repository/ThemeRepository.java
@@ -0,0 +1,10 @@
+package com.umc.cmap.domain.theme.repository;
+
+import com.umc.cmap.domain.theme.entity.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+    Optional<Theme> findByName(String name);
+}
