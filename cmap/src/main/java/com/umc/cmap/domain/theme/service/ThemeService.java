121ba43d1a07c7a4d411d9c07f4d27c9c6467bb2
wonseokyoon <01088115030a@gmail.com>
2023-07-20 20:24:01 +0900

    feat:add [cafe]theme.service

---
+++ cmap/src/main/java/com/umc/cmap/domain/theme/service/ThemeService.java
@@ -0,0 +1,51 @@
+package com.umc.cmap.domain.theme.service;
+
+import com.umc.cmap.config.BaseException;
+import com.umc.cmap.config.BaseResponse;
+import com.umc.cmap.config.BaseResponseStatus;
+import com.umc.cmap.domain.theme.entity.Theme;
+import com.umc.cmap.domain.theme.repository.ThemeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+public class ThemeService {
+    private final ThemeRepository themeRepository;
+
+    public ThemeService(ThemeRepository themeRepository) {
+        this.themeRepository = themeRepository;
+    }
+
+    public Theme createTheme(String themeName) {
+        Theme theme = new Theme();
+        theme.setName(themeName);
+
+        return themeRepository.save(theme);
+    }
+
+
+    public List<Theme> getAllThemes() {
+        return themeRepository.findAll();
+    }
+
+    public Theme getThemeByName(String name) throws BaseException {
+        return themeRepository.findByName(name)
+                .orElseThrow(() -> new BaseException(new BaseResponse<>(BaseResponseStatus.CAFE_THEME_NOT_FOUND)));
+    }
+
+    public Theme updateTheme(String name, String newThemeName) throws BaseException {
+        Theme theme = getThemeByName(name);
+        theme.setName(newThemeName);
+
+        return themeRepository.save(theme);
+    }
+
+    public void deleteTheme(String name) throws BaseException {
+        Theme theme = getThemeByName(name);
+        themeRepository.delete(theme);
+    }
+}
