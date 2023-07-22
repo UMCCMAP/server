53ba6e501c073d01ca9087e03318c9623d324748
yeonjy <yeonjypaper@gmail.com>
2023-07-22 18:58:09 +0900

    feat: add update review method

--- cmap/src/main/java/com/umc/cmap/domain/review/entity/Review.java
+++ cmap/src/main/java/com/umc/cmap/domain/review/entity/Review.java
@@ -1,6 +1,8 @@
 package com.umc.cmap.domain.review.entity;
 
+import com.umc.cmap.config.BaseTimeEntity;
 import com.umc.cmap.domain.cafe.entity.Cafe;
+import com.umc.cmap.domain.review.dto.ReviewRequest;
 import com.umc.cmap.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -11,13 +13,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Review {
+public class Review extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_idx")
     private Long idx;
 
-    @Embedded
-    private Content content;
+    String content;
 
     private Double score;
     private Boolean isDeleted;
@@ -31,11 +32,16 @@ public class Review {
     private Cafe cafe;
 
     @Builder
-    public Review(User user, Cafe cafe, Content content, Double score) {
+    public Review(User user, Cafe cafe, String content, Double score) {
         this.user = user;
         this.cafe = cafe;
         this.content = content;
         this.score = score;
         this.isDeleted = false;
+    }
+
+    public void update(ReviewRequest param){
+        this.content = param.getContent();
+        this.score = param.getScore();
     }
 }
