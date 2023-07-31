7ffc2dfced20bc3411cc17f86606c8d0c3d59bb8
seongddiyong <kodd1102@naver.com>
2023-07-31 21:15:30 +0900

    [#25] refactor: modify get all posts -> get posts without removed

--- cmap/src/main/java/com/umc/cmap/domain/board/repository/BoardRepository.java
+++ cmap/src/main/java/com/umc/cmap/domain/board/repository/BoardRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Page<Board> findAll(Pageable pageable);
+    Page<Board> findAllByRemovedAtIsNull(Pageable pageable);
 }
