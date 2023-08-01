47fe49ff2a66540cca16b05e3ffde2c46b15e66e
threeony <threeknot1250@cau.ac.kr>
2023-08-01 21:01:29 +0900

    feat: add exception handling and logout

--- cmap/src/main/java/com/umc/cmap/domain/user/login/controller/LoginController.java
+++ cmap/src/main/java/com/umc/cmap/domain/user/login/controller/LoginController.java
@@ -6,12 +6,16 @@ import com.umc.cmap.domain.user.login.service.CustomOAuth2UserService;
 import com.umc.cmap.domain.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -25,12 +29,12 @@ public class LoginController {
 
 
     @GetMapping("/users/login")
-    public String index(HttpServletRequest request){
+    public String login(){
         SessionUser loginUser = (SessionUser) httpSession.getAttribute("loginUser");
 
         if(loginUser!=null){
             Optional<User> user = userRepository.findByEmail(loginUser.getEmail());
-            if(user.get().getNickname() == null){
+            if(user.isPresent() && user.get().getNickname() == null){
                 //새로 가입한 유저는 닉네임을 받기 위해 이동
                 return "redirect:/users/nickname";
             }
@@ -38,9 +42,18 @@ public class LoginController {
         return "redirect/";
     }
 
+    @GetMapping("/users/nickname")
+    public String showNicknamePage(){
+        return "users/nickname";
+    }
 
     @PostMapping("/users/nickname")
-    public String nickname(@RequestParam("nickname") String nickname){
+    public String nickname(@NotNull @RequestParam("nickname") String nickname, RedirectAttributes redirectAttributes){
+        if (nickname.trim().isEmpty()) {
+            // 닉네임이 비어있는 경우
+            redirectAttributes.addFlashAttribute("errorMessage", "닉네임을 입력해주세요.");
+            return "redirect:/users/nickname";
+        }
 
         SessionUser loginUser = (SessionUser) httpSession.getAttribute("loginUser");
         userService.setNickname(loginUser.getEmail(), nickname);
