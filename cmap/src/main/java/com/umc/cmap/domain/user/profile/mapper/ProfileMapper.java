2a46f845b20f8ad2cde19f62c5426c5366472beb
threeony <threeknot1250@cau.ac.kr>
2023-08-03 03:16:35 +0900

    feat: add Profile Repository, Mapper, Service

---
+++ cmap/src/main/java/com/umc/cmap/domain/user/profile/mapper/ProfileMapper.java
@@ -0,0 +1,11 @@
+package com.umc.cmap.domain.user.profile.mapper;
+
+import com.umc.cmap.domain.user.entity.Profile;
+import com.umc.cmap.domain.user.profile.dto.ProfileResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProfileMapper {
+    ProfileResponse toResponse(Profile profile);
+}
