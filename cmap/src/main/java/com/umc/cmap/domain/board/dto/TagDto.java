76a847d8389901111e50cd948f5be5569b985715
seongddiyong <kodd1102@naver.com>
2023-08-03 22:11:50 +0900

    [#25] feat: checking-> about tag in board

---
+++ cmap/src/main/java/com/umc/cmap/domain/board/dto/TagDto.java
@@ -0,0 +1,19 @@
+package com.umc.cmap.domain.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TagDto {
+    private Long tagIdx;
+    private String tagName;
+
+    public TagDto(Long tagIdx, String tagName) {
+        this.tagIdx = tagIdx;
+        this.tagName = tagName;
+    }
+}
