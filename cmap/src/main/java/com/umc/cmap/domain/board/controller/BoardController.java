8ec89b4377055f0ee15fe2fb688001de3bccf3b1
seongddiyong <kodd1102@naver.com>
2023-08-07 01:21:03 +0900

    [#36] refactor: modify about warning

--- cmap/src/main/java/com/umc/cmap/domain/board/controller/BoardController.java
+++ cmap/src/main/java/com/umc/cmap/domain/board/controller/BoardController.java
@@ -5,7 +5,6 @@ import com.umc.cmap.config.BaseResponse;
 import com.umc.cmap.domain.board.dto.*;
 import com.umc.cmap.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
