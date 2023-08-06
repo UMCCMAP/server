dee17483ea65c42f3c0e011834ba2c4917fee9e6
threeony <threeknot1250@cau.ac.kr>
2023-08-06 17:18:13 +0900

    feat: modify getUserIdx to getUser

--- cmap/src/main/java/com/umc/cmap/domain/user/login/service/AuthService.java
+++ cmap/src/main/java/com/umc/cmap/domain/user/login/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.umc.cmap.domain.user.login.service;
 
+import com.umc.cmap.config.BaseException;
+import com.umc.cmap.config.BaseResponseStatus;
 import com.umc.cmap.domain.user.entity.User;
 import com.umc.cmap.domain.user.login.dto.SessionUser;
 import com.umc.cmap.domain.user.repository.UserRepository;
@@ -16,9 +18,10 @@ public class AuthService {
     private final HttpSession httpSession;
     private final UserRepository userRepository;
 
-    public Long getUserIdx(){
+    public User getUser() throws BaseException{
         SessionUser loginUser = (SessionUser) httpSession.getAttribute("loginUser");
-        Optional<User> user = userRepository.findByEmail(loginUser.getEmail());
-        return user.get().getIdx();
+        User user = userRepository.findByEmail(loginUser.getEmail())
+                    .orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
+        return user;
     }
 }
