71bd81f1e2168aa8adf7cad9fd6c598e91de61fc
yeonjy <yeonjypaper@gmail.com>
2023-08-06 17:49:09 +0900

    refactor: separate Review & ReviewImageService

--- cmap/src/main/java/com/umc/cmap/domain/review/service/ReviewService.java
+++ cmap/src/main/java/com/umc/cmap/domain/review/service/ReviewService.java
@@ -4,11 +4,14 @@ import com.umc.cmap.domain.cafe.entity.Cafe;
 import com.umc.cmap.domain.cafe.repository.CafeRepository;
 import com.umc.cmap.domain.review.dto.ReviewRequest;
 import com.umc.cmap.domain.review.dto.ReviewResponse;
+import com.umc.cmap.domain.review.dto.ReviewWriterResponse;
 import com.umc.cmap.domain.review.entity.Review;
-import com.umc.cmap.domain.review.mapper.ReviewImageMapper;
 import com.umc.cmap.domain.review.mapper.ReviewMapper;
-import com.umc.cmap.domain.review.repository.ReviewImageRepository;
 import com.umc.cmap.domain.review.repository.ReviewRepository;
+import com.umc.cmap.domain.user.entity.Profile;
+import com.umc.cmap.domain.user.entity.User;
+import com.umc.cmap.domain.user.repository.ProfileRepository;
+import com.umc.cmap.domain.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -22,33 +25,43 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewService {
 
+    private final ReviewImageService imageService;
     private final ReviewRepository reviewRepository;
-    private final ReviewImageRepository reviewImageRepository;
     private final CafeRepository cafeRepository;
+    private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
     private final ReviewMapper mapper;
-    private final ReviewImageMapper imageMapper;
 
 
     public List<ReviewResponse> getAll(Long id, Pageable pageable) {
-        return reviewRepository.findAllByCafeIdx(id, pageable).stream().filter(r -> !r.getIsDeleted())
-                .map(r -> mapper.toResponse(r, reviewImageRepository.findAllByReviewIdx(r.getIdx()))).toList();
+        List<Review> reviews = reviewRepository.findAllByCafeIdx(id, pageable).stream().filter(r -> !r.getIsDeleted()).toList();
+        return reviews.stream().map(r -> mapper.toResponse(r, imageService.getAll(r.getIdx()), getWriter(r.getUser()))).toList();
+
     }
 
     public ReviewResponse getOne(Long id) {
         Review review = reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        return mapper.toResponse(review, reviewImageRepository.findAllByReviewIdx(id));
+        return mapper.toResponse(review, imageService.getAll(id), getWriter(review.getUser()));
+    }
+
+    private ReviewWriterResponse getWriter(User user) {
+        return ReviewWriterResponse.builder()
+                .userIdx(user.getIdx())
+                .userNickname(user.getNickname())
+                .userImg(getWriterProfileImg(user.getIdx()))
+                .build();
+    }
+
+    private String getWriterProfileImg(Long userIdx) {
+        Profile profile = profileRepository.findByUserIdx(userIdx).orElseThrow(EntityNotFoundException::new);
+        return profile.getUserImg();
     }
 
     @Transactional
     public void save(Long cafeIdx, ReviewRequest param) {
         param.setCafe(getCafeEntity(cafeIdx));
-        Review review = reviewRepository.save(mapper.toEntity(param));  //유저 정보 추가해야 함
-        saveImages(param.getImageUrls(), review);
-    }
-
-    @Transactional
-    public void saveImages(List<String> urls, Review review) {
-        reviewImageRepository.save(imageMapper.toEntity(urls, review));
+        Review review = reviewRepository.save(mapper.toEntity(param, userRepository.findById(1L).get()));  //유저 정보 추가해야 함
+        imageService.saveAll(param.getImageUrls(), review);
     }
 
     private Cafe getCafeEntity(Long idx) {
@@ -62,11 +75,15 @@ public class ReviewService {
         updateReviewImages(param.getImageUrls(), review);
     }
 
-    @Transactional
-    public void updateReviewImages(List<String> urls, Review review) {
-        reviewImageRepository.deleteAll(reviewImageRepository.findAllByReviewIdx(review.getIdx()));
-        saveImages(urls, review);
+    private void updateReviewImages(List<String> urls, Review review) {
+        imageService.deleteAll(review);
+        imageService.saveAll(urls, review);
     }
 
+    @Transactional
+    public void delete(Long param) {
+        reviewRepository.findById(param).orElseThrow(EntityNotFoundException::new).delete();
+
+    }
 
 }
