627abb5a8a9b8339a258077e81da842a67147bed
seongddiyong <kodd1102@naver.com>
2023-08-09 22:09:50 +0900

    [#38] feat: this is for seyeon

--- cmap/src/main/java/com/umc/cmap/domain/board/repository/BoardRepository.java
+++ cmap/src/main/java/com/umc/cmap/domain/board/repository/BoardRepository.java
@@ -1,15 +1,18 @@
 package com.umc.cmap.domain.board.repository;
 
 import com.umc.cmap.domain.board.entity.Board;
+import com.umc.cmap.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<Board> findAllByRemovedAtIsNull(Pageable pageable);
     Page<Board> findByIdxInAndRemovedAtIsNull(List<Long> boardIdx, Pageable pageable);
     Page<Board> findByBoardTitleContainingOrBoardContentContainingAndRemovedAtIsNull(String boardTitle, String boardContent, Pageable pageable);
-
+    Long countByUserIdxAndRemovedAtIsNull(Long userIdx);
+    Optional<Board> findAllByUserIdxAndRemovedAtIsNull(Long userIdx);
 }
