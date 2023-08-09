4960f4643d9787b2b830504bf37a7359d9c3db14
threeony <threeknot1250@cau.ac.kr>
2023-08-09 21:00:11 +0900

    feat: add auto-add profile logic

--- cmap/src/main/java/com/umc/cmap/domain/user/login/controller/LoginController.java
+++ cmap/src/main/java/com/umc/cmap/domain/user/login/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.umc.cmap.domain.user.login.controller;
 
+import com.umc.cmap.config.BaseException;
+import com.umc.cmap.domain.user.entity.Profile;
 import com.umc.cmap.domain.user.entity.User;
 import com.umc.cmap.domain.user.login.dto.SessionUser;
+import com.umc.cmap.domain.user.login.service.AuthService;
 import com.umc.cmap.domain.user.login.service.CustomOAuth2UserService;
+import com.umc.cmap.domain.user.repository.ProfileRepository;
 import com.umc.cmap.domain.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -24,20 +28,22 @@ public class LoginController {
     private final HttpSession httpSession;
     private final UserRepository userRepository;
     private final CustomOAuth2UserService userService;
+    private final AuthService authService;
+    private final ProfileRepository profileRepository;
 
 
     @GetMapping("/users/login")
-    public String index(HttpServletRequest request){
+    public String login(){
         SessionUser loginUser = (SessionUser) httpSession.getAttribute("loginUser");
 
         if(loginUser!=null){
             Optional<User> user = userRepository.findByEmail(loginUser.getEmail());
-            if(user.get().getNickname() == null){
+            if(user.isPresent() && user.get().getNickname() == null){
                 //새로 가입한 유저는 닉네임을 받기 위해 이동
                 return "redirect:/users/nickname";
             }
         }
-        return "redirect/";
+        return "redirect:/";
     }
 
 
@@ -47,7 +53,7 @@ public class LoginController {
     }
 
     @PostMapping("/users/nickname")
-    public String nickname(@NotNull @RequestParam("nickname") String nickname, RedirectAttributes redirectAttributes){
+    public String nickname(@NotNull @RequestParam("nickname") String nickname, RedirectAttributes redirectAttributes) throws BaseException {
         if (nickname.trim().isEmpty()) {
             // 닉네임이 비어있는 경우
             redirectAttributes.addFlashAttribute("errorMessage", "닉네임을 입력해주세요.");
@@ -59,8 +65,11 @@ public class LoginController {
             return "redirect:/users/nickname";
         }
 
-        SessionUser loginUser = (SessionUser) httpSession.getAttribute("loginUser");
-        userService.setNickname(loginUser.getEmail(), nickname);
+        User user = authService.getUser();
+        userService.setNickname(user.getEmail(), nickname);
+
+        //프로필 생성
+        Profile profile = profileRepository.save(Profile.builder().user(user).build());
 
         return "redirect:/main";
     }
