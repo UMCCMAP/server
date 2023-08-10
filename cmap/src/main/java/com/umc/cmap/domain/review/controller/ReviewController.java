a069c722738873f39c132929afb4048b3a8bab04
yeonjy <yeonjypaper@gmail.com>
2023-08-11 02:58:57 +0900

    [#49] feat: add get review preview api & index -> idx

--- cmap/src/main/java/com/umc/cmap/domain/review/controller/ReviewController.java
+++ cmap/src/main/java/com/umc/cmap/domain/review/controller/ReviewController.java
@@ -1,8 +1,10 @@
 package com.umc.cmap.domain.review.controller;
 
 import com.umc.cmap.config.BaseException;
+import com.umc.cmap.domain.review.dto.ReviewPreviewResponse;
 import com.umc.cmap.domain.review.dto.ReviewRequest;
 import com.umc.cmap.domain.review.dto.ReviewResponse;
+import com.umc.cmap.domain.review.service.ReviewPreviewService;
 import com.umc.cmap.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
@@ -23,34 +25,41 @@ import static org.springframework.data.domain.Sort.Direction.DESC;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ReviewController {
     ReviewService reviewService;
+    ReviewPreviewService reviewPreviewService;
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{cafeIndex}/reviews")
-    public List<ReviewResponse> getAll(@PathVariable Long cafeIndex, @PageableDefault(sort = "createdAt", direction = DESC) Pageable pageable) {
-        return reviewService.getAll(cafeIndex, pageable);
+    @GetMapping("/{cafeIdx}/reviews")
+    public List<ReviewResponse> getAll(@PathVariable Long cafeIdx, @PageableDefault(sort = "createdAt", direction = DESC) Pageable pageable) {
+        return reviewService.getAll(cafeIdx, pageable);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/cafe-reviews/{reviewId}")
-    public ReviewResponse getOne(@PathVariable Long reviewId) {
-        return reviewService.getOne(reviewId);
+    @GetMapping("/cafe-reviews/{reviewIdx}")
+    public ReviewResponse getOne(@PathVariable Long reviewIdx) {
+        return reviewService.getOne(reviewIdx);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{cafeIdx}/preview")
+    public List<ReviewPreviewResponse> getPreview(@PathVariable Long cafeIdx, @PageableDefault(sort = "createdAt", direction = DESC) Pageable pageable) {
+        return reviewPreviewService.get(cafeIdx, pageable);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{cafeIndex}/review")
-    public void create(@PathVariable Long cafeIndex, @RequestBody @Valid final ReviewRequest dto) throws BaseException {
-        reviewService.save(cafeIndex, dto);
+    @PostMapping("/{cafeIdx}/review")
+    public void create(@PathVariable Long cafeIdx, @RequestBody @Valid final ReviewRequest dto) throws BaseException {
+        reviewService.save(cafeIdx, dto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/cafe-reviews/{reviewIndex}")
-    public void update(@PathVariable Long reviewIndex, @RequestBody @Valid final ReviewRequest dto) {
-        reviewService.update(reviewIndex, dto);
+    @PutMapping("/cafe-reviews/{reviewIdx}")
+    public void update(@PathVariable Long reviewIdx, @RequestBody @Valid final ReviewRequest dto) {
+        reviewService.update(reviewIdx, dto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/cafe-reviews/{reviewIndex}")
-    public void delete(@PathVariable Long reviewIndex) {
-        reviewService.delete(reviewIndex);
+    @DeleteMapping("/cafe-reviews/{reviewIdx}")
+    public void delete(@PathVariable Long reviewIdx) {
+        reviewService.delete(reviewIdx);
     }
 }
