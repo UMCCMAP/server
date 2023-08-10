21a9e3738137c7b6445593efaed88ca48b9c00c2
yeonjy <yeonjypaper@gmail.com>
2023-08-11 03:02:30 +0900

    [#49] feat: add get review preview service

---
+++ cmap/src/main/java/com/umc/cmap/domain/review/service/ReviewPreviewService.java
@@ -0,0 +1,48 @@
+package com.umc.cmap.domain.review.service;
+
+import com.umc.cmap.domain.review.dto.ReviewPreviewResponse;
+import com.umc.cmap.domain.review.entity.Review;
+import com.umc.cmap.domain.review.repository.ReviewImageRepository;
+import com.umc.cmap.domain.review.repository.ReviewRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class ReviewPreviewService {
+
+    ReviewService reviewService;
+    ReviewRepository reviewRepository;
+    ReviewImageRepository imageRepository;
+
+    public List<ReviewPreviewResponse> get(Long cafeIdx, Pageable pageable) {
+        List<Review> reviews = reviewRepository.findAllByCafeIdx(cafeIdx, pageable);
+        return reviews.stream().map(this::toResponse).toList();
+    }
+
+    private ReviewPreviewResponse toResponse(final Review review) {
+        return ReviewPreviewResponse.builder()
+                .userInfo(reviewService.getWriter(review.getUser()))
+                .content(review.getContent())
+                .imageUrl(getOneImageUrl(review.getIdx()))
+                .imageCnt(getImageCnt(review.getIdx()))
+                .createdAt(review.getCreatedAt())
+                .build();
+    }
+
+    private Long getImageCnt(Long reviewIdx) {
+        return imageRepository.countByReviewIdx(reviewIdx);
+    }
+
+    private String getOneImageUrl(Long reviewIdx) {
+        return imageRepository.findByReviewIdx(reviewIdx).getImageUrl();
+    }
+}
