b45ba6a72934862cf6e88fc53b97fd62ac30bbce
seongddiyong <kodd1102@naver.com>
2023-08-12 12:05:49 +0900

    [#53] refactor: remove unused import statments

--- cmap/src/main/java/com/umc/cmap/domain/board/repository/LikeBoardRepository.java
+++ cmap/src/main/java/com/umc/cmap/domain/board/repository/LikeBoardRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface LikeBoardRepository extends JpaRepository<LikeBoard, Long> {
     @Query("SELECT likeBoard FROM LikeBoard likeBoard WHERE likeBoard.board.idx = :boardIdx AND likeBoard.user.idx = :userIdx")
     LikeBoard findByBoardIdxAndUserIdx(@Param("boardIdx") Long boardIdx, @Param("userIdx") Long userIdx);
