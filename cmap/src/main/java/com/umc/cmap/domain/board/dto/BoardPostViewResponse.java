4207213ba9c8698e2fd0fd710257a46e8c605286
seongddiyong <kodd1102@naver.com>
2023-08-14 15:48:09 +0900

    [#60] feat: add count Comment at Board

--- cmap/src/main/java/com/umc/cmap/domain/board/dto/BoardPostViewResponse.java
+++ cmap/src/main/java/com/umc/cmap/domain/board/dto/BoardPostViewResponse.java
@@ -22,13 +22,14 @@ public class BoardPostViewResponse {
     private String boardTitle;
     private String boardContent;
     private Long cntLike;
+    private Long cntComment;
     private List<HashMap<Long, String>> tagList;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private boolean like;
     private boolean canModifyPost;
 
-    public BoardPostViewResponse(Board board, String profileImg, Long cntLike, List<HashMap<Long, String>> tagList, boolean like, boolean canModifyPost) {
+    public BoardPostViewResponse(Board board, String profileImg, Long cntLike, Long cntComment, List<HashMap<Long, String>> tagList, boolean like, boolean canModifyPost) {
         this.idx = board.getIdx();
         this.cafeName = board.getCafe().getName();
         this.nickName = board.getUser().getNickname();
@@ -36,6 +37,7 @@ public class BoardPostViewResponse {
         this.boardTitle = board.getBoardTitle();
         this.boardContent = board.getBoardContent();
         this.cntLike = cntLike;
+        this.cntComment = cntComment;
         this.tagList = tagList;
         this.createdAt = board.getCreatedAt();
         this.updatedAt = board.getUpdatedAt();
