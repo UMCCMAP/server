2c37311915dedef0297d391d90219d6eda37fd40
seongddiyong <kodd1102@naver.com>
2023-08-14 15:48:09 +0900

    [#60] feat: add count Comment at Board

--- cmap/src/main/java/com/umc/cmap/domain/comment/repository/CommentRepository.java
+++ cmap/src/main/java/com/umc/cmap/domain/comment/repository/CommentRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByBoardIdx(Long boardIdx, Pageable pageable);
+    Long countByUserIdx(Long userIdx);
 }
