73fed2bdb063deee383783a7745371dff2d82cf3
threeony <threeknot1250@cau.ac.kr>
2023-08-16 08:21:02 +0900

    [#85] feat: add find mates logic

--- cmap/src/main/java/com/umc/cmap/domain/cmap/service/CmapService.java
+++ cmap/src/main/java/com/umc/cmap/domain/cmap/service/CmapService.java
@@ -1,29 +1,38 @@
 package com.umc.cmap.domain.cmap.service;
 
+import com.umc.cmap.config.BaseException;
+import com.umc.cmap.config.BaseResponseStatus;
 import com.umc.cmap.domain.cmap.repository.CmapRepository;
+import com.umc.cmap.domain.user.entity.Mates;
+import com.umc.cmap.domain.user.entity.User;
+import com.umc.cmap.domain.user.profile.dto.MatesInfoMapping;
+import com.umc.cmap.domain.user.repository.MatesRepository;
+import com.umc.cmap.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly=true)
 public class CmapService {
     private final CmapRepository cmapRepository;
+    private final MatesRepository matesRepository;
+    private final UserRepository userRepository;
 
-    /**
-     * 노깨 공간
-     */
+    public List<String> getMates(String userNickname) throws BaseException{
+        User user = userRepository.findByNickname(userNickname)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
 
-    /**
-     * 젼 공간
-     */
-
-    /**
-     * 션 공간
-     */
-
-    /**
-     * 데옹 공간
-     */
+        List<Mates> matesList = matesRepository.findAllByFromIdx(user.getIdx());
+        List<String> matesNicknameList = new ArrayList<>();
+        for(Mates mates: matesList){
+            String matesInfo = userRepository.findByIdx(mates.getTo().getIdx()).get().getNickname();
+            matesNicknameList.add(matesInfo);
+        }
+        return matesNicknameList;
+    }
 }
