b1a9eede1f1338935c456929df9db58631b3c01d
yeonjy <yeonjypaper@gmail.com>
2023-08-20 04:55:05 +0900

    !Hotfix: add User class import

--- cmap/src/main/java/com/umc/cmap/domain/cmap/repository/CmapRepository.java
+++ cmap/src/main/java/com/umc/cmap/domain/cmap/repository/CmapRepository.java
@@ -3,6 +3,7 @@ package com.umc.cmap.domain.cmap.repository;
 import com.umc.cmap.domain.cafe.entity.Cafe;
 import com.umc.cmap.domain.cmap.entity.Cmap;
 import com.umc.cmap.domain.cmap.entity.Type;
+import com.umc.cmap.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
