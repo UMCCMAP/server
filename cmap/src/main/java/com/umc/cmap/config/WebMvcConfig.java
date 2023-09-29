7f75cbc4f34f1ca9c6721e7d32097603779b885f
yeonjy <81320703+yeonjy@users.noreply.github.com>
2023-09-30 04:21:53 +0900

    refactor: restoration allowed methods

--- cmap/src/main/java/com/umc/cmap/config/WebMvcConfig.java
+++ cmap/src/main/java/com/umc/cmap/config/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
